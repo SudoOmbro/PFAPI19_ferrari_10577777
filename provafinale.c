33c010d74e37892a670deb7e63521e54e8893a33
SudoOmbro <10577777@polimi.it>
2019-06-19 15:53:35 +0200

    a

--- provafinale.c
+++ provafinale.c
@@ -2,9 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+//type definitions--------------------------------------------------------------
+
+typedef char String[20];
+
+typedef struct {
+  String name;
+  void* first_popular_entity;
+  void* table_next;
+} Relation;
+
+typedef struct {
+  Relation* relation;
+  void* dest_entity;
+  void* table_next;
+} Relation_in_entity;
+
+typedef struct {
+  String name;
+  Relation_in_entity out_relations_table[500];
+  void* table_next;
+} Entity;
+
+typedef struct {
+  Entity* entity;
+  void* list_next;
+} Popular_entity;
+
+Entity entity_table[80000];
+Relation relation_table[30];
+
+//------------------------------------------------------------------------------
+
 int main(int argc, char const *argv[])
 {
-  if (argc > 2)
+  if (argc == 2)
   {
     //program
   }
