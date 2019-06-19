43b95e3c9653df548f5afcf49fc3309dd6fd99c5
SudoOmbro <10577777@polimi.it>
2019-06-19 17:45:46 +0200

    hi

--- provafinale.c
+++ provafinale.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+//command definitions-----------------------------------------------------------
+
+static const char command_report[] = "report"; //case 0
+static const char command_addent[] = "addent"; //case 1
+static const char command_delent[] = "delent"; //case 2
+static const char command_addrel[] = "addrel"; //case 3
+static const char command_delrel[] = "delrel"; //case 4
+static const char command_end[] = "end";       //case 5
+
 //type definitions--------------------------------------------------------------
 
-typedef char String[20];
+typedef char String[50];
+typedef char LongString[200];
 
 typedef struct {
   String name;
@@ -33,12 +43,64 @@ Entity entity_table[80000];
 Relation relation_table[30];
 
 //------------------------------------------------------------------------------
+//functions definitions
 
-int main(int argc, char const *argv[])
+int hash_function(int table_size, String text)
 {
-  if (argc == 2)
+  int value;
+  return value;
+}
+
+Entity* create_entity()
+{
+  Entity* self;
+  return self;
+}
+
+char generate_opcode(LongString input_string)
+{
+  //guarda i primi 6 caratteri della stringa in input per generare l'opcode
+  char opcode;
+  return opcode;
+}
+
+//------------------------------------------------------------------------------
+int main() //main program
+{
+  LongString input_string;
+  char opcode = 0;
+  while (true)
   {
-    //program
+    scanf("%s", input_string);
+    opcode = generate_opcode(input_string);
+    switch (opcode)
+    {
+      case 0:
+      {
+
+      }
+      case 1:
+      {
+
+      }
+      case 2:
+      {
+
+      }
+      case 3:
+      {
+
+      }
+      case 4:
+      {
+
+      }
+      case 5:
+      {
+        break;
+      }
+    }
   }
+
   return 0;
 }
